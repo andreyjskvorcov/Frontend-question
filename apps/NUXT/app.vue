b5241fd0b4211580e0757bf21fc7897e536ab087
Andrej Skvorcov <andrejskvorocov@gmail.com>
2026-01-29 15:40:09 +0300

    init2

---
+++ apps/NUXT/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <NuxtLayout>
+    <div class="flex p-6 w-full pt-20">
+      Content
+      
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+// Базовый service для fetch
+abstract class BaseService {
+  abstract getData(): Promise<any>
+}
+
+// Расширяем конкретным сервисом
+class UserService extends BaseService {
+  async getData() {
+    return await $fetch('/api/users')
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ apps/NUXT/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="common-layout">
+    <el-header
+      class="mx-auto flex items-center justify-between p-6 bg-white shadow-sm w-full fixed"
+    >
+      Header
+    </el-header>
+
+    <slot></slot>
+
+    <el-footer class="footer flex p-6 w-full">Footer</el-footer>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
